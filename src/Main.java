e0e84a5fbe0f1dd9ebd0fdabf1a83d8b7fed6dce
kokutoukiritsugu <kokutoukiritsugu@gmail.com>
2017-04-11 17:11:51 +0800

    not completed

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import org.ini4j.Config;
+import org.ini4j.Ini;
+import org.ini4j.Profile.Section;
+import org.ini4j.Wini;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String filename = args.length > 0 ? args[0] : "1.ini";
+
+        Ini ini = new Wini();
+        Ini ini2 = new Wini();
+
+        Config config = new Config();
+        config.setFileEncoding(Charset.forName("utf-16le"));
+        config.setStrictOperator(true);
+        config.setEscape(false);
+        config.setMultiOption(true);
+        config.setMultiSection(true);
+
+        ini.setConfig(config);
+        ini2.setConfig(config);
+
+        ini.load(new File(filename));
+
+        for (String sectionName : ini.keySet()) {
+            List<Section> sectionList = ini.getAll(sectionName);
+            for (Section section : sectionList) {
+                for (String optionName : section.keySet()) {
+                    List<String> optionList = section.getAll(optionName);
+                    for (String optionVaule : optionList) {
+                        System.out.printf("%s %s %s\n", sectionName, optionName, optionVaule);
+                    }
+                }
+            }
+
+            Section section = ini.get(sectionName);
+            ini2.add(sectionName.toLowerCase());
+            Section section2 = ini2.get(sectionName.toLowerCase());
+            for (String optionKey : section.keySet()) {
+                section2.add(optionKey.toLowerCase(), section.get(optionKey));
+            }
+
+        }
+
+        String fileNameBefore = "1";
+        FileOutputStream fos = new FileOutputStream(fileNameBefore + filename);
+        OutputStreamWriter osw = new OutputStreamWriter(fos, Charset.forName("utf-16le"));
+        osw.write("\uFEFF");
+        ini2.store(osw);
+
+
+    }
+}
